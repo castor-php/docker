1ede87bb27e86158a5ba59d05f55832c16897a4a
Joel Wurtz <joel.wurtz@gmail.com>
2024-06-03 23:15:12 +0200

    feat(logs): allow logs command to work without tty

--- src/functions.php
+++ src/functions.php
@@ -7,6 +7,7 @@ use Castor\Attribute\AsTask;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Process\Exception\ExceptionInterface;
 
+use Symfony\Component\Process\Process;
 use function Castor\context;
 use function Castor\io;
 use function Castor\variable;
@@ -105,13 +106,19 @@ function logs(
     #[AsOption(mode: InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED)]
     array $profiles = [],
 ): void {
-    $command = ['logs', '-f', '--tail', '150'];
+    $command = ['logs', '--tail', '150'];
+    $c = context();
+
+    if (Process::isTtySupported()) {
+        $c = $c->withTty();
+        $command[] = '-f';
+    }
 
     if ($service) {
         $command[] = $service;
     }
 
-    docker_compose($command, c: context()->withTty(), profiles: $profiles);
+    docker_compose($command, c: $c, profiles: $profiles);
 }
 
 #[AsTask(description: 'Lists containers status', aliases: ['ps'], namespace: 'docker')]
@@ -139,73 +146,3 @@ function destroy(
 
     docker_compose(['down', '--remove-orphans', '--volumes', '--rmi=local']);
 }
-//
-//#[AsContext(default: true)]
-//function create_default_context(): Context
-//{
-//    $data = [
-//        'project_name' => 'app',
-//        'root_domain' => 'app.test',
-//        'extra_domains' => [],
-//        'project_directory' => 'application',
-//        'php_version' => '8.2',
-//        'docker_compose_files' => [
-//            'docker-compose.yml',
-//            'docker-compose.worker.yml',
-//        ],
-//        'macos' => false,
-//        'power_shell' => false,
-//        // check if posix_geteuid is available, if not, use getmyuid (windows)
-//        'user_id' => \function_exists('posix_geteuid') ? posix_geteuid() : getmyuid(),
-//        'root_dir' => \dirname(__DIR__),
-//    ];
-//
-//    if (file_exists($data['root_dir'] . '/infrastructure/docker/docker-compose.override.yml')) {
-//        $data['docker_compose_files'][] = 'docker-compose.override.yml';
-//    }
-//
-//    // We need an empty context to run command, since the default context has
-//    // not been set in castor, since we ARE creating it right now
-//    $emptyContext = new Context();
-//
-//    $data['composer_cache_dir'] = cache('composer_cache_dir', function () use ($emptyContext): string {
-//        $composerCacheDir = capture(['composer', 'global', 'config', 'cache-dir', '-q'], onFailure: '', context: $emptyContext);
-//        // If PHP is broken, the output will not be a valid path but an error message
-//        if (!is_dir($composerCacheDir)) {
-//            $composerCacheDir = sys_get_temp_dir() . '/castor/composer';
-//            // If the directory does not exist, we create it. Otherwise, docker
-//            // will do, as root, and the user will not be able to write in it.
-//            if (!is_dir($composerCacheDir)) {
-//                mkdir($composerCacheDir, 0o777, true);
-//            }
-//        }
-//
-//        return $composerCacheDir;
-//    });
-//
-//    $platform = strtolower(php_uname('s'));
-//    if (str_contains($platform, 'darwin')) {
-//        $data['macos'] = true;
-//        $data['docker_compose_files'][] = 'docker-compose.docker-for-x.yml';
-//    } elseif (\in_array($platform, ['win32', 'win64', 'windows nt'])) {
-//        $data['docker_compose_files'][] = 'docker-compose.docker-for-x.yml';
-//        $data['power_shell'] = true;
-//    }
-//
-//    if ($data['user_id'] > 256000) {
-//        $data['user_id'] = 1000;
-//    }
-//
-//    if (0 === $data['user_id']) {
-//        log('Running as root? Fallback to fake user id.', 'warning');
-//        $data['user_id'] = 1000;
-//    }
-//
-//    return new Context(
-//        $data,
-//        pty: Process::isPtySupported(),
-//        environment: [
-//            'BUILDKIT_PROGRESS' => 'plain',
-//        ]
-//    );
-//}
