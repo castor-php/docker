ba306c92883e179c3da34cbf0dd403b5eb0d1b63
Joel Wurtz <joel.wurtz@gmail.com>
2025-12-12 09:30:20 +0100

    feat(dockerfile): allow to include / extends template in twig

--- twig-dockerfile/build.go
+++ twig-dockerfile/build.go
@@ -3,12 +3,17 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"net"
+	"net/rpc"
 	"os/exec"
 
+	"github.com/moby/buildkit/client/llb"
 	dockerfile "github.com/moby/buildkit/frontend/dockerfile/builder"
 	"github.com/moby/buildkit/frontend/gateway/client"
+	"github.com/pkg/errors"
 )
 
 func build(ctx context.Context, c client.Client) (*client.Result, error) {
@@ -19,28 +24,170 @@ func build(ctx context.Context, c client.Client) (*client.Result, error) {
 		return nil, err
 	}
 
-	transform := func(dockerfile []byte) ([]byte, error) {
-		cmd := exec.CommandContext(ctx, "castor", "transform-docker-file", string(jsonString))
-		cmd.Stdin = bytes.NewReader(dockerfile)
+	transform, err := CreateTransform(jsonString, ctx, c)
 
-		var stdout, stderr bytes.Buffer
-		cmd.Stdout = &stdout
-		cmd.Stderr = &stderr
-
-		if err := cmd.Run(); err != nil {
-			if stderr.Len() > 0 {
-				return nil, fmt.Errorf("%s", stderr.String())
-			}
-
-			return nil, err
-		}
-
-		return stdout.Bytes(), nil
+	if err != nil {
+		return nil, err
 	}
-	if err := InjectDockerfileTransform(transform, c); err != nil {
+
+	if err := InjectDockerfileTransform(transform.transformDockerfile, c); err != nil {
 		return nil, err
 	}
 
 	// Pass control to the upstream Dockerfile frontend
 	return dockerfile.Build(ctx, c)
+}
+
+type Transform struct {
+	options []byte
+	client  client.Client
+}
+
+func (s *Transform) Hi(name string, r *string) error {
+	*r = fmt.Sprintf("Hello, %s!", name)
+	return nil
+}
+
+func (s *Transform) transformDockerfile(dockerfile []byte) ([]byte, error) {
+	cmd := exec.Command("castor", "transform-docker-file", string(s.options))
+	cmd.Stdin = bytes.NewReader(dockerfile)
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if err := cmd.Run(); err != nil {
+		if stderr.Len() > 0 {
+			return nil, fmt.Errorf("%s", stderr.String())
+		}
+
+		return nil, err
+	}
+
+	return stdout.Bytes(), nil
+}
+
+func CreateTransform(options []byte, ctx context.Context, client client.Client) (*Transform, error) {
+	ln, err := net.Listen("tcp", ":6001")
+
+	if err != nil {
+		return nil, err
+	}
+
+	transform := new(Transform)
+	transform.options = options
+	transform.client = client
+
+	err = rpc.Register(transform)
+
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				continue
+			}
+			_ = conn
+
+			go func() {
+				// ensure connection is closed after use
+				defer func(conn net.Conn) {
+					err := conn.Close()
+					if err != nil {
+						panic(err)
+					}
+				}(conn)
+
+				var length uint32
+
+				for {
+					err := binary.Read(conn, binary.BigEndian, &length)
+
+					if err != nil {
+						return
+					}
+
+					buf := make([]byte, length)
+
+					err = binary.Read(conn, binary.BigEndian, &buf)
+
+					if err != nil {
+						return
+					}
+
+					// lookup for context file
+					var request LoadFileRequest
+					err = json.Unmarshal(buf, &request)
+
+					if err != nil {
+						return
+					}
+
+					fileContent, err := loadFileFromContext(ctx, client, request.Context, request.Filename)
+
+					if err != nil {
+						return
+					}
+
+					responseLength := uint32(len(fileContent))
+
+					err = binary.Write(conn, binary.BigEndian, responseLength)
+
+					if err != nil {
+						return
+					}
+
+					err = binary.Write(conn, binary.BigEndian, fileContent)
+
+					if err != nil {
+						return
+					}
+				}
+			}()
+		}
+	}()
+
+	return transform, nil
+}
+
+type LoadFileRequest struct {
+	Context  string `json:"context"`
+	Filename string `json:"filename"`
+}
+
+func loadFileFromContext(ctx context.Context, c client.Client, localCtx string, filename string) ([]byte, error) {
+	src := llb.Local(
+		localCtx,
+		llb.IncludePatterns([]string{filename}),
+		llb.SessionID(c.BuildOpts().SessionID),
+		llb.SharedKeyHint(defaultDockerfileName),
+	)
+	def, err := src.Marshal(context.TODO())
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal local source")
+	}
+	res, err := c.Solve(ctx, client.SolveRequest{
+		Definition: def.ToPB(),
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create solve request")
+	}
+	ref, err := res.SingleRef()
+	if err != nil {
+		return nil, err
+	}
+
+	var xdockerfile []byte
+	xdockerfile, err = ref.ReadFile(ctx, client.ReadRequest{
+		Filename: filename,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read dockerfile '%s': %s\n", filename, err)
+	}
+
+	return xdockerfile, nil
 }
--- twig-dockerfile/castor.php
+++ twig-dockerfile/castor.php
@@ -1,6 +1,7 @@
 <?php
 
 use Castor\Attribute\AsTask;
+use Spiral\Goridge;
 
 use function Castor\io;
 
@@ -12,6 +13,16 @@ function transform_docker_file(string $options): void
 
     // parse options build args
     $args = [];
+
+    // create tcp connection to golang rpc server
+    $socket = fsockopen("127.0.0.1", 6001, $errno, $errstr, 30);
+
+    if (!$socket) {
+        echo "Error: $errstr ($errno)\n";
+        exit(1);
+    }
+
+    stream_set_timeout($socket, 1);
 
     foreach ($options as $key => $value) {
         // can be like 'build-arg:KEY' => 'value'
@@ -30,30 +41,105 @@ function transform_docker_file(string $options): void
         }
     }
 
-    $loader = new \Twig\Loader\ArrayLoader([
-        'Dockerfile' => $dockerfile,
-    ]);
+    $loader = new RpcTwigLoader($socket, $dockerfile);
     $twig = new \Twig\Environment($loader);
 
 
     echo $twig->render('Dockerfile', $args);
     echo "\n";
-
-    foreach ($options as $key => $value) {
-        dump_dockerfile("Build arg: " . $key . "==" . $value);
-    }
-
-    dump_dockerfile("Current pwd: " . getcwd());
-
-    // list files in current directory recursively
-    $finder = \Castor\finder()->in("/mnt");
-
-    foreach ($finder->files() as $file) {
-        dump_dockerfile("File: " . $file->getPath());
-    }
 }
 
 function dump_dockerfile(string $value): void
 {
     echo "RUN echo '" . $value . "'\n";
+}
+
+class RpcTwigLoader implements \Twig\Loader\LoaderInterface
+{
+    private array $templates = [];
+
+    public function __construct(
+        private $socket,
+        string $content
+    )
+    {
+        $this->templates['Dockerfile'] = new \Twig\Source($content, "Dockerfile");
+    }
+
+    public function getSourceContext(string $name): \Twig\Source
+    {
+        $source = $this->loadTemplate($name);
+
+        if ($source !== null) {
+            return $source;
+        }
+
+        throw new \Twig\Error\LoaderError(sprintf('Template "%s" is not defined.', $name));
+    }
+
+    public function exists(string $name): bool
+    {
+        return $this->loadTemplate($name) !== null;
+    }
+
+    public function getCacheKey(string $name): string
+    {
+        return $name;
+    }
+
+    public function isFresh(string $name, int $time): bool
+    {
+        return true;
+    }
+
+    private function loadTemplate(string $name): ?\Twig\Source
+    {
+        if (isset($this->templates[$name])) {
+            return $this->templates[$name];
+        }
+
+        $context = "context";
+
+        if (str_starts_with($name, "@")) {
+            $parts = explode("/", $name, 2);
+            $context = substr($parts[0], 1); // remove @
+            $name = $parts[1] ?? "";
+        }
+
+        $content = $this->getFileFromServer($context, $name);
+
+        if ($content === "") {
+            return null;
+        }
+
+        $this->templates[$name] = new \Twig\Source($content, $name);
+
+        return $this->templates[$name];
+    }
+
+    private function getFileFromServer(string $context, string $filename): string
+    {
+        $message = json_encode([
+            'context' => $context,
+            'filename' => $filename,
+        ]);
+
+        // pack message length + message
+        $length = strlen($message);
+        $packed = pack('N', $length) . $message;
+
+        // send to socket
+        $written = fwrite($this->socket, $packed);
+
+        // read response length
+        $lengthData = fread($this->socket, 4);
+
+        if (!$lengthData) {
+            return "";
+        }
+
+        $responseLength = unpack('N', $lengthData)[1];
+
+        return fread($this->socket, $responseLength);
+    }
 }
